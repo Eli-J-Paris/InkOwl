9d43017072a82c429c13b333ac95c2bc42a6184f
Eli-J-Paris <Eli.J.Paris@gmail.com>
2023-11-06 13:02:01 -0700

    Add project files.

---
+++ InkOwl/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InkOwl.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
