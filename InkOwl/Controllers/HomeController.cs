9edccb9616b572b5ba8f2446b57c9431cbe6ecbb
Eli-J-Paris <Eli.J.Paris@gmail.com>
2023-11-07 08:37:52 -0700

    added modal popup for each nest

--- InkOwl/Controllers/HomeController.cs
+++ InkOwl/Controllers/HomeController.cs
@@ -16,12 +16,52 @@ namespace InkOwl.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var nests = _context.Nests.ToList();
+            return View(nests);
         }
 
+        [HttpPost]
+        [Route("/nest/new")]
+        public IActionResult CreateNest()
+        {
+            //create a new untitled nest
+            var nest = new Nest { Title ="Untitled", CreatedAt = DateTime.Now.ToUniversalTime()};
+            _context.Nests.Add(nest);
+            _context.SaveChanges();
+            return Redirect("/home");
+        }
 
+        [Route("/nest/{q}")]
+        public IActionResult ShowNest(string q)
+        {
+            var nest = _context.Nests.Find(q);
+            return View(nest);
+        }
 
+        [HttpPost]
+        [Route("/nest/edit/{id}")]
+        public IActionResult EditNest(int id, string title)
+        {
+            var nest = _context.Nests.Find(id);
+            nest.Title = title;
+            _context.Nests.Update(nest);
+            _context.SaveChanges();
 
+            return Redirect("/home");
+
+        }
+
+        [HttpPost]
+        [Route("/nest/delete/{id}")]
+        public IActionResult DeleteNest(int id)
+        {
+            var nest = _context.Nests.Find(id);
+            _context.Nests.Remove(nest);
+            _context.SaveChanges();
+
+            return Redirect("/home");
+
+        }
 
 
         //public IActionResult Privacy()
--- InkOwl/Models/Nest.cs
+++ InkOwl/Models/Nest.cs
@@ -4,8 +4,7 @@
     {
         public int Id { get; set; }
         public string? Title { get; set; }
-        public DateTime CreatedAt { get; set; }
-        
+        public DateTime CreatedAt { get; set; } 
         public List<Article> Articles { get; set; } = new List<Article>();
         public List<TextDoc> Notes { get; set; } = new List<TextDoc>();
     }
